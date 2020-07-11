e45e3f001e62d60123b5f55a20d06f7a9710f1f9
Blessing jarawani <blessingjarawani@gmail.com>
2020-07-11 11:58:49 +0200

    Theory

--- DemoTests/CalculatorTests.cs
+++ DemoTests/CalculatorTests.cs
@@ -1,21 +1,22 @@
 using BLL;
 using System;
+using System.Transactions;
 using Xunit;
 
 namespace DemoTests
 {
     public class CalculatorTests
     {
-        [Fact]
-        public void Add_SimpleValuesShouldCalculate()
+        [Theory]
+        [InlineData(4,3,7)]
+        [InlineData(21, 5.25, 26.25)]
+        [InlineData(double.MaxValue, 5, double.MaxValue)]
+        public void Add_SimpleValuesShouldCalculate(double x, double y, double expected)
         {
             //Arrange
-            var firstNumber = 2;
-            var secondNumber = 3;
-            var expected = 5;
-
+          
             // Act 
-            var actual = Calculator.Add(firstNumber, secondNumber);
+            var actual = Calculator.Add(x,y);
             // Assert
             Assert.Equal(expected, actual);
         }
